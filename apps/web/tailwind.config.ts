efc4d166d2eab7f4598fc76e3ecc7be76a4d040a
Philipp Trentmann <philipp@bluegardenproject.com>
2025-03-10 15:36:53 +0100

    remove all unnecessary tailwind config files

--- apps/web/tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "tailwindcss"
-import sharedConfig from "@turbo-with-tailwind-v4/tailwind-config"
-
-const config: Pick<Config, "content" | "presets"> = {
-  content: ["./src/app/**/*.tsx"],
-  presets: [sharedConfig],
-}
-
-export default config
--- packages/ui/tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "tailwindcss"
-import sharedConfig from "@turbo-with-tailwind-v4/tailwind-config"
-
-const config: Pick<Config, "prefix" | "presets" | "content"> = {
-  content: ["./src/**/*.tsx"],
-  presets: [sharedConfig],
-}
-
-export default config
